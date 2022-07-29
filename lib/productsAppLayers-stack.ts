ff19b52842303210f4dc6d5a51eeff94d1542a6f
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-07-29 16:59:00 -0300

    feat: add productsEvents layer on productsAppLayers stack

--- lib/productsAppLayers-stack.ts
+++ lib/productsAppLayers-stack.ts
@@ -4,23 +4,34 @@ import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
 
 export class ProductsAppLayersStack extends Stack {
-  readonly productsLayers: LayerVersion;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    this.productsLayers = new LayerVersion(this, 'ProductsLayer',
+    const productsLayers = new LayerVersion(this, 'ProductsLayer',
     {
       code: Code.fromAsset('lambda/products/layers/productsLayer'),
       compatibleRuntimes: [Runtime.NODEJS_14_X],
       layerVersionName: 'ProductsLayer',
       removalPolicy: RemovalPolicy.RETAIN
     });
-    
     new StringParameter(this, 'ProductsLayerVersionArn',
     {
       parameterName: 'ProductsLayerVersionArn',
-      stringValue: this.productsLayers.layerVersionArn
+      stringValue: productsLayers.layerVersionArn
+    });
+
+    const productsEventsLayers = new LayerVersion(this, 'ProductsEventsLayer',
+    {
+      code: Code.fromAsset('lambda/products/layers/productsEventsLayer'),
+      compatibleRuntimes: [Runtime.NODEJS_14_X],
+      layerVersionName: 'ProductsEventsLayer',
+      removalPolicy: RemovalPolicy.RETAIN
+    });
+    new StringParameter(this, 'ProductsEventsLayerVersionArn',
+    {
+      parameterName: 'ProductsEventsLayerVersionArn',
+      stringValue: productsEventsLayers.layerVersionArn
     });
   }
 }
