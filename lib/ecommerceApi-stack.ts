f6f19782f728aa01a927ba352ba4f7a90e098e33
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-08-16 17:57:40 -0300

    feat: add new class private method createOrdersService on Api stack

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -1,13 +1,14 @@
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { AccessLogFormat, LambdaIntegration, 
-  LogGroupLogDestination, RestApi } from 'aws-cdk-lib/aws-apigateway';
+  LogGroupLogDestination, RequestValidator, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 import { LogGroup } from 'aws-cdk-lib/aws-logs';
 
 interface ECommerceApiStackProps extends StackProps {
   productsFetchHandler: NodejsFunction
   productsAdminHandler: NodejsFunction
+  ordersHandler: NodejsFunction
 }
 
 export class ECommerceApiStack extends Stack {
@@ -36,6 +37,39 @@ export class ECommerceApiStack extends Stack {
     });
 
     this.createProductsService(props, api);
+    this.createOrdersService(props, api);
+  }
+
+  private createOrdersService(props: ECommerceApiStackProps, api: RestApi) {
+    const ordersIntegration = new LambdaIntegration(props.ordersHandler);
+
+    //resource - /orders
+    const ordersResource = api.root.addResource('orders');
+
+    //GET /orders
+    //GET /orders?email=paul@email.com
+    //GET /orders?email=paul@email.com&orderId=123
+    ordersResource.addMethod('GET', ordersIntegration);
+
+    const orderDeletionValidator = new RequestValidator(this, 'OrderDeletionValidator',
+    {
+      restApi: api,
+      requestValidatorName: 'OrderDeletionValidator',
+      validateRequestParameters: true
+    });
+
+    //DELETE /orders?email=paul@email.com&orderId=123
+    ordersResource.addMethod('DELETE', ordersIntegration,
+    {
+      requestParameters: {
+        'method.request.querystring.email': true,
+        'method.request.querystring.orderId': true
+      },
+      requestValidator: orderDeletionValidator
+    });
+
+    //POST /orders
+    ordersResource.addMethod('POST', ordersIntegration);
   }
 
   private createProductsService(props: ECommerceApiStackProps, api: RestApi) {
